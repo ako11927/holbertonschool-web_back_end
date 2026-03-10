3525bab5da562495e405d44c470238fcdf3eed50
ako11927 <11927@holbertonstudents.com>
2026-03-10 03:17:07 +0000

    doing tasks ES6 Classes

---
+++ ES6_classes/9-hoisting.js
@@ -0,0 +1,9 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+}
+
+export class StudentHolberton extends HolbertonCourse {}
---
+++ ES6_classes/README.md
@@ -0,0 +1,3 @@
+Hello every one this is AhmedK doing the holberton School
+web back end
+ES6_classes
