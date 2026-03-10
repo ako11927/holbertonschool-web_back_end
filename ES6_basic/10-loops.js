07a11542addcaa16cfe6e86462601126b241660a
ako11927 <11927@holbertonstudents.com>
2026-03-10 02:33:11 +0000

    fixing tasks

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -2,6 +2,6 @@ export default function appendToEachArrayValue(array, appendString) {
   for (const [index, value] of array.entries()) {
     array[index] = appendString + value;
   }
+
   return array;
 }
-
